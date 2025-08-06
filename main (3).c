915f4cc7d386157386002d56cc9a747de44b6327
Santiago cunha <k06323334@gmail.com>
2025-08-06 17:26:39 -0300

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    
+    int x;
+    float y;
+    float s;
+    
+    x=30;
+    y=2.2;
+    s=x/y;
+
+    printf("A divisao dos valores x e y eh:%f", s);
+
+    return 0;
+}
